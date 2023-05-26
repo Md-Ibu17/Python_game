e4494b44de8d797420e71b161a7d890e14cbdbbd
md_ibu <mdibu2817@gmail.com>
2023-05-26 18:43:14 +0530

    Added character animation and added blocks/terrain

--- tutorial.py
+++ tutorial.py
@@ -2,6 +2,8 @@ import os
 import math 
 import pygame
 import random
+
+from pygame.sprite import Group
 '''
 Below two lines is for,
 instead of dynamilcally loading each file and images
@@ -49,11 +51,22 @@ def Load_sprite_sheets(dir1,dir2,width,height,direction=False):
     return all_sprites
      
 #####END#####
+##block function##
+def get_block(size):
+    path = join("assets","Terrain","Terrain.png")
+    image = pygame.image.load(path).convert_alpha()
+    surface = pygame.Surface((size,size),pygame.SRCALPHA,32)
+    rect = pygame.Rect(96,0,size,size)
+    surface.blit(image,(0,0),rect)
+    return pygame.transform.scale2x(surface)
+###END####
 #GENERATING GAME PLAYER:
 class Player(pygame.sprite.Sprite):
     COLOR = (0,250,0)
     GRAVITY = 1
     SPRITES = Load_sprite_sheets("MainCharacters","NinjaFrog",32,32,True)
+    ANIMATION_DELAY = 2
+
     def __init__(self,x,y,width,height):
         self.rect=pygame.Rect(x,y,width,height)
         self.x_vel = 0
@@ -82,13 +95,50 @@ class Player(pygame.sprite.Sprite):
     def loop(self,fps):
         #self.y_vel += min(1,(self.fall_count/fps)*self.GRAVITY)
         self.move(self.x_vel,self.y_vel)
+        self.update_sprite()
         self.fall_count +=1 
+   #ANIMATING THE PLAYER:
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = (self.animation_count//self.ANIMATION_DELAY)%len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_count+=1
+        self.update()
+
+    def update(self):
+        self.rect = self.sprite.get_rect(topleft=(self.rect.x,self.rect.y))
+        self.mask = pygame.mask.from_surface(self.sprite)
+
 
     def draw(self,win):
-        self.sprite = self.SPRITES["idle_"+ self.direction][0]
         win.blit(self.sprite,(self.rect.x,self.rect.y))
 ##### END#######
 
+### Another class ###
+class Object(pygame.sprite.Sprite):
+    def __init__(self,x,y,width,height,name=None):
+        super().__init__()
+        self.rect = pygame.Rect(x,y,width,height)
+        self.image = pygame.Surface((width,height),pygame.SRCALPHA)
+        self.width = width
+        self.height = height
+        self.name = name
+    def draw(self,win):
+        win.blit(self.image,(self.rect.x,self.rect.y))
+####END#########
+
+####ADDING TERRAIN AND BLOCK####
+class Block(Object):
+    def __init__(self, x, y,size):
+        super().__init__(x, y,size,size)
+        block = get_block(size)
+        self.image.blit(block,(0,0))
+        self.mask = pygame.mask.from_surface(self.image)
+#####END########
 #MOVING THE PLAYER##
 def handle_move(player):
     keys = pygame.key.get_pressed()
@@ -114,10 +164,15 @@ def get_background(name):
 
 
  #Creating a function to draw the background:
-def draw(window,background,bg_image,player):
+def draw(window,background,bg_image,player,objects):
     for tile in background:
         window.blit(bg_image,tile)
+    
+    for obj in objects:
+        obj.draw(window)
+    
     player.draw(window)
+    
     pygame.display.update()            
 
 
@@ -126,8 +181,11 @@ def draw(window,background,bg_image,player):
 def main(window):
     clock = pygame.time.Clock()
     background,bg_image = get_background("Blue.png")
+    
+    block_size = 96
 
     player = Player(100,100,50,50)
+    floor = [Block(i*block_size,HEIGHT-block_size,block_size) for i in range(-WIDTH//block_size,(WIDTH*2)//block_size)]
     run = True
     while run:
         clock.tick(FPS)
@@ -139,7 +197,7 @@ def main(window):
 
         player.loop(FPS)
         handle_move(player)
-        draw(window,background,bg_image,player)
+        draw(window,background,bg_image,player,floor)
     pygame.quit()
     quit()
 if __name__== "__main__":
